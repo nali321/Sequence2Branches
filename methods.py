f644d188b62da38d6935050c2b216ed5e8bd8676
Numair <numairali321@gmail.com>
2023-05-01 12:03:07 -0500

    dbug

--- methods.py
+++ methods.py
@@ -73,6 +73,7 @@ def closest_leaves(newick, size, nametodata, outgroup):
     for x in distances:
         if len(little_leaves) < size:
             name = disttoname[x]
+            print(name)
             little_leaves.append(nametodata[name])
 
     #create the gtotree text files for smaller tree
